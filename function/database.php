3e40849b536b8c039a7f0203cb3b980663023da6
parisyashp <147698243+parisyashp@users.noreply.github.com>
2025-05-24 22:32:03 +0700

    Update database.php
    Modify these methods in your Database class:

--- function/database.php
+++ function/database.php
@@ -21,34 +21,35 @@ class Database{
 		return $result;
 	}
 	
-    public function tambah_tamu($nama, $handphone, $alamat){
-		$tgl = date('Y-m-d');
-		//Memeriksa tamu dengan no handphone dan tanggal apakah data exist
-		$cek = $this->conn->query("SELECT count(*) as jumlah from daftar_tamu where hp='$handphone' and substring(tanggal,1,10)='$tgl'");
-		$d = mysqli_fetch_assoc($cek);
-		if($d['jumlah'] > 0){
-			$result[] = array('status' => 'exist');
-		}else{ 
-			$query = $this->conn->query("INSERT INTO daftar_tamu(nama,hp,alamat,tanggal)VALUES('$nama', '$handphone', '$alamat', now())");
-			if ($query= true){
-				$result[] = array('status' => 'success');
-			}else{
-				$result[] = array('status' => 'gagal');
-			}
-		}
-		return $result;
+	public function tambah_tamu($nama, $handphone, $alamat, $instansi, $tujuan_kunjungan){
+	    $tgl = date('Y-m-d');
+	    // Check if visitor exists
+	    $cek = $this->conn->query("SELECT count(*) as jumlah from daftar_tamu where hp='$handphone' and substring(tanggal,1,10)='$tgl'");
+	    $d = mysqli_fetch_assoc($cek);
+	    if($d['jumlah'] > 0){
+	        $result[] = array('status' => 'exist');
+	    }else{ 
+	        $query = $this->conn->query("INSERT INTO daftar_tamu(nama,hp,alamat,instansi,tujuan_kunjungan,tanggal) 
+	                    VALUES('$nama', '$handphone', '$alamat', '$instansi', '$tujuan_kunjungan', now())");
+	        if ($query = true){
+	            $result[] = array('status' => 'success');
+	        }else{
+	            $result[] = array('status' => 'gagal');
+	        }
+	    }
+	    return $result;
 	}
 	public function daftar_tamu(){
-		$hasil = $this->conn->query("SELECT * from daftar_tamu");
-		$cek = mysqli_num_rows($hasil); 
-		if($cek > 0){
-			while ($d=mysqli_fetch_assoc($hasil)){
-				$result[] = $d;
-			}
-		}else{
-			$result[] = null;
-		}
-		return $result;
+	    $hasil = $this->conn->query("SELECT id,nama,hp,alamat,instansi,tujuan_kunjungan,tanggal from daftar_tamu");
+	    $cek = mysqli_num_rows($hasil); 
+	    if($cek > 0){
+	        while ($d=mysqli_fetch_assoc($hasil)){
+	            $result[] = $d;
+	        }
+	    }else{
+	        $result[] = null;
+	    }
+	    return $result;
 	}
 	public function daftar_tamu_g_tanggal(){
 		$hasil = $this->conn->query("SELECT substring(tanggal,1,10)as tanggal, count(*)as data from daftar_tamu group by substring(tanggal, 1,10) order by tanggal asc limit 7");
